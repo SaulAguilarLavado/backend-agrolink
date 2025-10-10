b9680f3426e13bed2423b040b5dae60f13822556
SaulAguilarLavado <u22304350@utp.edu.pe>
2025-10-10 10:28:02 -0500

    Creando backend del propyecto AgroLink

---
+++ src/main/java/com/proy/utp/backend_agrolink/BackendAgrolinkApplication.java
@@ -0,0 +1,13 @@
+package com.proy.utp.backend_agrolink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAgrolinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAgrolinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proy/utp/backend_agrolink/BackendAgrolinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proy.utp.backend_agrolink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAgrolinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
